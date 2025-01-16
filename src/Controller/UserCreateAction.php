2ad34f92d6a55e4294cf60c19ab84eeee0a930ed
abdulla <tuychiyevabdulla5@gmail.com>
2025-01-16 11:51:01 +0500

    Create UserCreateAction.php

---
+++ src/Controller/UserCreateAction.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class UserCreateAction extends AbstractController
+{
+    public function __invoke(): void
+    {
+        print 'Hello World!';
+        exit;
+    }
+}
